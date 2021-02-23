083cf0aa2493276e38cb0f1b077608975d4f997f
Allie <alliecaton3@gmail.com>
2021-02-23 09:35:56 -0500

    commit tree c826b86582b40fddb9f7cba6e4a8645f10701ef2

---
+++ db/migrate/20210223143231_update_columns_with_foreign_keys.rb
@@ -0,0 +1,6 @@
+class UpdateColumnsWithForeignKeys < ActiveRecord::Migration[5.0]
+  def change
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
+  end
+end
